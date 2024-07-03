291d8b9d76d2516308efc33c5fac3740543ca35f
milirmo <milagritosromero.mr@gmail.com>
2024-07-02 22:54:21 -0300

    último commit antes de entregar. Terminé de acomodar las cards del inicio, hice los detalles del footer y acomodé las rutas a todas las demás secciones a las que te lleva el inicio.

--- src/app/modules/producto/producto-routing.module.ts
+++ src/app/modules/producto/producto-routing.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LibrosComponent } from './page/libros/libros.component';
 import { OfertasComponent } from './page/ofertas/ofertas.component';
+import { TodosproductosComponent } from './page/todosproductos/todosproductos.component';
 
 const routes: Routes = [
 
   //routeo al modulo producto > libros
   { 
     path: 'libros', component: LibrosComponent
+  },
+
+  //routeo al modulo producto > todo
+  { 
+    path: 'todos-los-productos', component: TodosproductosComponent
   },
 
   //routeo al modulo producto > ofertas y combos
