ce23db0e54e063e6059107f26f387cb0c12e7860
milirmo <milagritosromero.mr@gmail.com>
2024-07-02 22:41:10 -0300

    terminé de acomodar el footer y las cards del inicio

--- src/app/modules/inicio/components/card/card.component.ts
+++ src/app/modules/inicio/components/card/card.component.ts
@@ -22,27 +22,27 @@ export class CardComponent {
       },
       {
         uid: "",
-        titulo: "Harry Potter y la piedra filosofal",
-        genero: "Novela / Fantasía",
-        autor: "J.K Rowling",
-        img: "https://cdnx.jumpseller.com/queleola/image/10413614/resize/540/540?1602090638",
-        precio: 9000
+        titulo: "Indias Blancas",
+        genero: "Novela / Historia",
+        autor: "Florencia Bonelli",
+        img: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1327292662l/13421452.jpg",
+        precio: 7000
       },
       {
         uid: "",
-        titulo: "Harry Potter y la piedra filosofal",
-        genero: "Novela / Fantasía",
+        titulo: "Animales Fantásticos y Dónde Encontrarlos",
+        genero: "Fantasía / Estudio",
         autor: "J.K Rowling",
-        img: "https://cdnx.jumpseller.com/queleola/image/10413614/resize/540/540?1602090638",
-        precio: 9000
+        img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1253335238i/2490849.jpg",
+        precio: 11000
       },
       {
         uid: "",
-        titulo: "Harry Potter y la piedra filosofal",
-        genero: "Novela / Fantasía",
-        autor: "J.K Rowling",
-        img: "https://cdnx.jumpseller.com/queleola/image/10413614/resize/540/540?1602090638",
-        precio: 9000
+        titulo: "La Trilogía de New York",
+        genero: "Novela / Suspenso / Policial",
+        autor: "Paul Auster",
+        img: "https://www.planetadelibros.com/usuaris/libros/fotos/301/original/portada_la-trilogia-de-nueva-york_paul-auster_201907181317.jpg",
+        precio: 10000
       },
 
     ]
