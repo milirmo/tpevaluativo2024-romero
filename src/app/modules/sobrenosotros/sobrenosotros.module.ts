635d7284a8d7b931598e98c00f38fd3dd5dab5f5
milirmo <milagritosromero.mr@gmail.com>
2024-10-08 10:04:43 -0300

    mejoré diseño y UX de páginas productos

---
+++ src/app/modules/sobrenosotros/sobrenosotros.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SobrenosotrosRoutingModule } from './sobrenosotros-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SobrenosotrosRoutingModule
+  ]
+})
+export class SobrenosotrosModule { }
